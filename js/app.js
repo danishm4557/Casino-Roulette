2ff43f581c86930106634467010d6b30411b3d7e
danishm4557 <danishm4557@gmail.com>
2021-12-13 20:01:45 -0600

    Added all buttons to the table to cordinate with each bet number

--- js/app.js
+++ js/app.js
@@ -1,1 +1,12 @@
 console.log('PROJECT 1: ROULETTE');
+
+const instructionsButton = document.querySelector('#instructions')
+const instructionsText = document.querySelector('.instructionsDiv')
+
+instructionsButton.addEventListener('click', ()=> {
+  if (instructionsText.style.opacity == 100) {
+    instructionsText.style.opacity = 0
+  } else if (instructionsText.style.opacity == 0) {
+    instructionsText.style.opacity = 100
+  }
+})
